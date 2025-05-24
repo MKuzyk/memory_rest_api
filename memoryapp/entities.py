75ff47fddbe643788cb9bcdd0aff1a468c3ef741
Sebastian Słomian <slomian@wszib.edu.pl>
2025-05-24 15:34:47 +0200

    Lista fiszek

--- memoryapp/entities.py
+++ memoryapp/entities.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 from dataclasses_json import dataclass_json
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from memoryapp import db
@@ -13,3 +14,15 @@ class Category(db.Model):
 
     category_name: Mapped[str]
     category_id: Mapped[int] = mapped_column(primary_key=True)
+
+
+@dataclass_json
+@dataclass
+class Card(db.Model):
+    __tablename__ = 'cards'
+
+    word: Mapped[str]
+    definition: Mapped[str]
+    card_id: Mapped[int] = mapped_column(primary_key=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey(Category.category_id, ondelete='CASCADE'), nullable=False)
+
--- memoryapp/repository.py
+++ memoryapp/repository.py
@@ -1,5 +1,5 @@
 from memoryapp import db
-from memoryapp.entities import Category
+from memoryapp.entities import Category, Card
 
 
 def get_categories() -> list[Category]:
@@ -21,4 +21,11 @@ def delete_category(category_id: int):
     if category:
         db.session.delete(category)
         db.session.commit()
+
+
+def get_cards(category_id: int):
+    db.get_or_404(Category, category_id)
+    return Card.query.filter_by(category_id=category_id).all()
+
+
 
